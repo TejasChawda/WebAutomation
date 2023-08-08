71d1a2384caf9dfc443a1ed3ec5f66593528e7e4
Admin <admin@Admins-MacBook-Pro.local>
2023-08-08 18:12:02 +0530

    trying ci/cd pipeline

---
+++ src/test/java/Demo.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+public class Demo {
+
+    @Test
+    public void should(){
+        WebDriver driver;
+        //Arrange
+
+
+        //Act
+
+        //Assert
+    }
+
+
+}
