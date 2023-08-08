335bbf03826e3dab456dcf9cf0f31e62ab28101b
Admin <admin@Admins-MacBook-Pro.local>
2023-08-08 17:36:33 +0530

    initial commit

---
+++ src/main/java/org/example/LauncherPage.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+
+public class LauncherPage {
+    public LauncherPage(WebDriver webDriver) {
+    }
+    public void navigateTo(String url) {
+    }
+}
+
